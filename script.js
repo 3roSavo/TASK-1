a48b6ab14b5cc8b19e3b11653f4e6cca57a737c8
Eros Savogin <eros.sav@gmail.com>
2024-04-23 23:41:37 +0200

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const boxImages = document.getElementById("container")
 
 console.log("prova stampa")
 
-const arrayOfImages = [
+const arrayOfImages = [  /* per risparmiare spazio nell'applicazione utilizzerò degli url assoluti presenti sul mio profilo cloudinary */
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709741665/Progetto_Parco/Galleria_Foto_Escursioni/siywhqnfockxcr5o4mxm.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709726017/Progetto_Parco/Galleria_Foto_Escursioni/g4swwdqhponplud4lakv.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709726044/Progetto_Parco/Galleria_Foto_Escursioni/zymhxhma6ggy06efpd17.png",
@@ -11,7 +11,7 @@ const arrayOfImages = [
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709717859/Progetto_Parco/Galleria_Foto_Escursioni/vplzed4t8lcxehiiyceh.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709735804/Progetto_Parco/Galleria_Foto_Escursioni/ee6es9bmvmagrwsgqpqk.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709735896/Progetto_Parco/Galleria_Foto_Escursioni/rc0zbaem37n0dpohfvyn.png",
-    "https://res.cloudinary.com/diklzegyw/image/upload/v1709717888/Progetto_Parco/Galleria_Foto_Escursioni/h3sao34wneezn1cjkvua.jpg",    // uwyeduweydguwyegd
+    "https://res.cloudinary.com/diklzegyw/image/upload/v1709717888/Progetto_Parco/Galleria_Foto_Escursioni/h3sao34wneezn1cjkvua.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709726019/Progetto_Parco/Galleria_Foto_Escursioni/o5scqu8xmcd3b7wb8tsu.jpg",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709724577/Progetto_Parco/Galleria_Foto_Escursioni/hizltz7jjxjrcfxjvwe6.webp",
     "https://res.cloudinary.com/diklzegyw/image/upload/v1709716614/Progetto_Parco/Galleria_Foto_Escursioni/ziii1efofgfbd0zwxahx.jpg"
